4f3e71b50582f13e875727b10a123f2015c764d0
courtney08 <court1307@gmail.com>
2025-03-26 22:22:13 -0500

    Added Hero and Shield Scripts

---
+++ Assets/__Scripts/Shield.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+public class Shield : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public float rotationsPerSecond = 0.1f;
+
+    [Header("Dynamic")]
+    public int levelShown = 0;
+
+    Material mat;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+        
+        if(levelShown != currLevel){
+            levelShown = currLevel;
+            mat.mainTextureOffset = new Vector2(0.2f*levelShown, 0);
+        }
+        float rZ = -(rotationsPerSecond*Time.time*360) % 360f;
+        transform.rotation = Quaternion.Euler(0,0,rZ);
+    }
+}
