88d625c1e8daab1e9205ab77dbda90f2a8d56628
courtney08 <court1307@gmail.com>
2025-03-27 11:37:01 -0500

    Updated Hero Script with function

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -36,5 +36,11 @@ public class Hero : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(vAxis*pitchMult, hAxis*rollMult,0);
     }
+    
+    void OnTriggerEnter(Collider other){
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        Debug.Log("Shield trigger hit by: " +go.gameObject.name);
+    }
 
 }
