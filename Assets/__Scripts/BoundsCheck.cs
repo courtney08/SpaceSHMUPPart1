266af1c6d69650e25eab6ccdbfe187b71e38e9d2
courtney08 <court1307@gmail.com>
2025-03-26 21:01:06 -0500

    Added to BoundsCheck Script

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class BoundsCheck : MonoBehaviour
 {
+    public enum eType {center, inset, outset};
+
+    [Header("Inscribed")]
+    public eType boundsType = eType.center;
+    public float radius = 1f;
+
     [Header("Dynamic")]
     public float camWidth;
     public float camHeight;
@@ -12,20 +18,24 @@ public class BoundsCheck : MonoBehaviour
     }
     
     void LateUpdate(){
+        float checkRadius = 0;
+        if(boundsType == eType.inset) checkRadius = -radius;
+        if(boundsType == eType.outset) checkRadius = radius;
+
         Vector3 pos = transform.position;
 
-        if(pos.x > camWidth){
-            pos.x = camWidth;
+        if(pos.x > camWidth + checkRadius){
+            pos.x = camWidth + checkRadius;
         }
-        if(pos.x < -camWidth){
-            pos.x = -camWidth;
+        if(pos.x < -camWidth - checkRadius){
+            pos.x = -camWidth - checkRadius;
         }
 
-        if(pos.y > camHeight){
-            pos.y = camHeight;
+        if(pos.y > camHeight + checkRadius){
+            pos.y = camHeight + checkRadius;
         }
-        if(pos.y < -camHeight){
-            pos.y = -camHeight;
+        if(pos.y < -camHeight - checkRadius){
+            pos.y = -camHeight - checkRadius;
         }
 
         transform.position = pos;
