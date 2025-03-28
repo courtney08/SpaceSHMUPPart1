f61541b0d4cc577581e7e8bd921d037b7e94a7c6
courtney08 <court1307@gmail.com>
2025-03-28 15:02:41 -0500

    Added to Enemy_4 Script
    pg 887

--- Assets/__Scripts/Enemy_4.cs
+++ Assets/__Scripts/Enemy_4.cs
@@ -5,19 +5,53 @@ using System.Collections;
 [RequireComponent(typeof(EnemyShield))]
 public class Enemy_4 : Enemy
 {
+    [Header("Enemy_4 Inscribed Fields")]
+    public float duration = 4;
     private EnemyShield[] allShields;
     private EnemyShield thisShield;
+    private Vector3 p0, p1;
+    private float timeStart;
 
     
     void Start()
     {
         allShields = GetComponentsInChildren<EnemyShield>();
         thisShield = GetComponent<EnemyShield>();
+
+        p0 = p1 = pos;
+        InitMovement();
+    }
+
+    void InitMovement(){
+        p0 = p1;
+
+        float widMinRad = bndCheck.camWidth - bndCheck.radius;
+        float hgtMinRad = bndCheck.camHeight - bndCheck.radius;
+        p1.x = Random.Range(-widMinRad, widMinRad);
+        p1.y = Random.Range(-hgtMinRad, hgtMinRad);
+
+        if(p0.x * p1.x > 0 && p0.y * p1.y > 0){
+            if(Mathf.Abs(p0.x) > Mathf.Abs(p0.y)){
+                p1.x *= -1;
+            }
+            else{
+                p1.y *= -1;
+            }
+        }
+
+        timeStart = Time.time;
     }
 
     public override void Move(){
-        //
-        //
+        float u = (Time.time - timeStart) / duration;
+
+        if(u >= 1){
+            InitMovement();
+            u = 0;
+        }
+
+        u = u - 0.15f * Mathf.Sin(u * 2 * Mathf.PI);
+        pos = (1-u)*p0 + u*p1;
     }
 
 
