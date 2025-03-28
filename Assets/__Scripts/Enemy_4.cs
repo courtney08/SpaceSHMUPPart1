d29c8be7ea75b4eb4216aa48b90c7c1f19c800c0
courtney08 <court1307@gmail.com>
2025-03-28 14:48:27 -0500

    Updated Enemy_4 Script
    pg 885

--- Assets/__Scripts/Enemy_4.cs
+++ Assets/__Scripts/Enemy_4.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
-public class Enemy_4 : MonoBehaviour
+[RequireComponent(typeof(EnemyShield))]
+public class Enemy_4 : Enemy
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private EnemyShield[] allShields;
+    private EnemyShield thisShield;
+
+    
     void Start()
     {
-        
+        allShields = GetComponentsInChildren<EnemyShield>();
+        thisShield = GetComponent<EnemyShield>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override void Move(){
+        //
+        //
+    }
+
+
+    void OnCollisionEnter(Collision coll){
+        GameObject otherGO = coll.gameObject;
+
+        ProjectileHero p = otherGO.GetComponent<ProjectileHero>();
+        if(p != null){
+            Destroy(otherGO);
+
+            if(bndCheck.isOnScreen){
+                GameObject hitGO = coll.contacts[0].thisCollider.gameObject;
+            
+
+            float dmg = Main.GET_WEAPON_DEFINITION(p.type).damageOnHit;
+
+            bool shieldFound = false;
+            foreach(EnemyShield es in allShields){
+                if(es.gameObject == hitGO){
+                    es.TakeDamage(dmg);
+                    shieldFound = true;
+                }
+            }
+            if(!shieldFound) thisShield.TakeDamage(dmg);
+
+            if(thisShield.isActive) return;
+
+            if(!calledShipDestroyed){
+                Main.SHIP_DESTROYED(this);
+                calledShipDestroyed = true;
+            }
+
+            
+                Destroy(gameObject);
+            }
+        }
+        else{
+            Debug.Log("Enemy_4 hit by non-ProjectileHero: " + otherGO.name);
+        }
     }
 }
